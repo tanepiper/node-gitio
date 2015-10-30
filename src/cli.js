e6b201e593b29087d2b90f2d22d04c6e3816a98d
Tane Piper <piper.tane@gmail.com>
2015-10-30 17:54:29 +0000

    Looking to improve the cli with more options

--- src/cli.js
+++ src/cli.js
@@ -4,20 +4,42 @@
 import gitio from './index';
 import optimist from 'optimist';
 
-let program = optimist
-  .usage('Shorten URLs like the cool kids\ngitio https://github.com/<path> [optional code]')
-  .wrap(80)
-  .demand(1);
 
-let argv = program.argv;
-let url = argv._[0];
-let code = argv._[1];
+let process = () => {
+  let program = optimist
+    .usage('Usage: $0 user/repo [-u github url] [-c code]')
+    .alias('u', 'url')
+    .alias('c', 'code')
+    .wrap(80)
+    .argv;
 
-gitio(url, code).then(
-  (url) => console.log(url),
-  (error) => {
-    console.error('** Error:', error.message);
-    console.error();
-    console.error(program.help());
+  let url = '';
+  let code = program.c ? program.c : false;
+
+  if (program.u) {
+    url = program.u;
+  } else {
+    if (program._[0]) {
+      if (/^(https:|http:)\/\/(github\.com)\/[A-Za-z0-9\?&=\-_\/]+$/.test(program._[0])) {
+        url = program._[0];
+      } else {
+        url = 'https://github.com/' + program._[0];
+      }
+    }
   }
-);
+
+  if (!url) {
+    return console.error(`${program.$0}: No URL passed`);
+  }
+
+  gitio(url, code).then(
+    (url) => console.log(url),
+    (error) => {
+      console.error(`${program.$0}: Error - ${error.message}`);
+      console.error();
+      console.error(program.help());
+    }
+  );
+};
+
+process();
